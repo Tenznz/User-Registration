315ec9b5da4192d9a57062889d5b359a5389844e
tenznz <dhugkar@gmail.com>
2021-11-24 01:54:16 +0530

    Added: Display method

--- src/com/bridgelabz/user/registration/UserResgistration.java
+++ src/com/bridgelabz/user/registration/UserResgistration.java
@@ -1,24 +1,29 @@
 package com.bridgelabz.user.registration;
 
 public class UserResgistration {
+	private String firstName, lastName, email, password, phoneNumber;
 
 	public static void main(String[] args) {
-		String firstName, lastName, email, password, phoneNumber;
-		// First Name Adding
+		UserResgistration user = new UserResgistration();
 		Pattern pattern = new Pattern();
-		pattern.addFirstName();
-		firstName = pattern.getFirstName();
-		// Last Name Adding
-		pattern.addLastName();
-		lastName = pattern.getLastName();
-		// Email Adding
-		pattern.addEmail();
-		email = pattern.getEmail();
-		// Email Adding
-		pattern.addPhoneNumber();
-		phoneNumber = pattern.getPhoneNumber();
-		System.out.println(
-				"\nUser Data:-\n\tFirst Name : " + firstName + "\n\tLast Name : " + lastName + "\n\tEmail : " + email+ "\n\tPhone Number : " + phoneNumber);
+		
+		pattern.addFirstName(); // First Name Adding
+		user.firstName = pattern.getFirstName();
+
+		pattern.addLastName(); // Last Name Adding
+		user.lastName = pattern.getLastName();
+
+		pattern.addEmail(); // Email Adding
+		user.email = pattern.getEmail();
+
+		pattern.addPhoneNumber(); // Phone Number Adding
+		user.phoneNumber = pattern.getPhoneNumber();
+		user.display();
+	}
+
+	private void display() {
+		System.out.println("\nUser Data:-\n\tFirst Name : " + firstName + "\n\tLast Name : " + lastName + "\n\tEmail : "
+				+ email + "\n\tPhone Number : " + phoneNumber);
 	}
 
 }
