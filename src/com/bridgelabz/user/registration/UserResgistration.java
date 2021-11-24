c4f5df4996c1d65ae2c14bc342b6f8a6399e7647
tenznz <dhugkar@gmail.com>
2021-11-24 11:06:36 +0530

    Password Atleast 1 Upper Case

--- src/com/bridgelabz/user/registration/UserResgistration.java
+++ src/com/bridgelabz/user/registration/UserResgistration.java
@@ -2,32 +2,31 @@ package com.bridgelabz.user.registration;
 
 public class UserResgistration {
 
-	
-		private String firstName, lastName, email, password, phoneNumber;
+	private String firstName, lastName, email, password, phoneNumber;
 
-		public static void main(String[] args) {
-			UserResgistration user = new UserResgistration();
-			Pattern pattern = new Pattern();
-			
-			pattern.addFirstName(); // First Name Adding
-			user.firstName = pattern.getFirstName();
+	public static void main(String[] args) {
+		UserResgistration user = new UserResgistration();
+		Pattern pattern = new Pattern();
 
-			pattern.addLastName(); // Last Name Adding
-			user.lastName = pattern.getLastName();
+		pattern.addFirstName(); // First Name Adding
+		user.firstName = pattern.getFirstName();
 
-			pattern.addEmail(); // Email Adding
-			user.email = pattern.getEmail();
+		pattern.addLastName(); // Last Name Adding
+		user.lastName = pattern.getLastName();
 
-			pattern.addPhoneNumber(); // Phone Number Adding
-			user.phoneNumber = pattern.getPhoneNumber();
-			
-			pattern.addPassword(); // Password Adding
-			user.password = pattern.getPassword();
-			user.display();
-		}
+		pattern.addEmail(); // Email Adding
+		user.email = pattern.getEmail();
 
-		private void display() {
-			System.out.println("\nUser Data:-\n\tFirst Name : " + firstName + "\n\tLast Name : " + lastName + "\n\tEmail : "
-					+ email + "\n\tPhone Number : " + phoneNumber+ "\n\tPassword : " + password);
-		}
+		pattern.addPhoneNumber(); // Phone Number Adding
+		user.phoneNumber = pattern.getPhoneNumber();
+
+		pattern.addPassword(); // Password Adding
+		user.password = pattern.getPassword();
+		user.display();
+	}
+
+	private void display() {
+		System.out.println("\nUser Data:-\n\tFirst Name : " + firstName + "\n\tLast Name : " + lastName + "\n\tEmail : "
+				+ email + "\n\tPhone Number : " + phoneNumber + "\n\tPassword : " + password);
+	}
 }
